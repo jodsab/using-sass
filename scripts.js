e3a1c3be621c66991d0da65668c025a32910b655
jodsab <85377591+jodsab@users.noreply.github.com>
2021-06-28 15:18:32 -0500

    op2

--- scripts.js
+++ scripts.js
@@ -54,26 +54,9 @@ function scrollSectionLeft(){
     })
 }
 
-//CALCULADORA
-
-
-let showmsj = true;
-let calculator = document.querySelector("#calculator");
-
-function pagoSelect(){
-    var metpago = document.getElementById("pago").value;
-    if(metpago == "MMR"){
-        document.getElementById("calculator").innerHTML = `<p>¿Cuánto MMR te gustaría comprar? </br>(Max: 3000 MMR)</p><input type="number" min="0" max="3000" step="30">`;
-    }
-    else if(metpago =="Presupuesto"){
-        document.getElementById("calculator").innerHTML = `<p>¿Cuánto es tu presupuesto? </br>(Max: S/360.00)</p><input type="number" min="0" max="360" step="30">`
-    }
-    else{
-        document.getElementById("calculator").innerHTML = `<p>Seleccione una opción...</p>`;
-    }
-}
 
 function openMenu(){
     let menu = document.querySelector("#opMenu");
     menu.classList.toggle("show");
 }
+
